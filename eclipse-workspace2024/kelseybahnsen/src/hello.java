a67f45f8207a354b08e38ca8ea93459fd855bf64
kjbahnsen688 <157331076+kjbahnsen688@users.noreply.github.com>
2024-01-22 09:50:25 -0500

    Add files via upload

---
+++ eclipse-workspace2024/kelseybahnsen/src/hello.java
@@ -0,0 +1,10 @@
+//kelsey 1/22 github
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println ("i got a new laptop so i don't have my first folder");
+		System.out.println ("hopefully this will fulfill this assignment");
+
+	}
+
+}
